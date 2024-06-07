8e51752ce21b41de93b11f3036b058144ce85489
Sergey <Salvadory@ya.ru>
2024-06-07 18:33:08 +0600

    Update Comporate-distr.sh

--- Scripts/Comporate-distr.sh
+++ Scripts/Comporate-distr.sh
@@ -1,45 +1,26 @@
 #!/bin/bash
 
-# Убедимся, что передано два аргумента
-if [ "$#" -ne 2 ]; then
-    echo "Использование: $0 <директория1> <директория2>"
-    exit 1
-fi
+# Две директории для сравнения
+dir1="path/to/first/directory"
+dir2="path/to/second/directory"
 
-dir1=$1
-dir2=$2
+# Функция для сравнения файлов
+compare_files() {
+    file1=$1
+    file2=$2
 
-# Проверяем, что обе директории существуют
-if [ ! -d "$dir1" ]; then
-    echo "Директория $dir1 не существует"
-    exit 1
-fi
-
-if [ ! -d "$dir2" ]; then
-    echo "Директория $dir2 не существует"
-    exit 1
-fi
-
-# Функция для создания контрольной суммы для каждого файла
-generate_checksums() {
-    dir=$1
-    find "$dir" -type f -exec md5sum {} \; | sort -k 1
+    # Сравнение содержимого файлов
+    if cmp -s "$file1" "$file2"; then
+        echo "Файлы $file1 и $file2 идентичны"
+    else
+        echo "Файлы $file1 и $file2 отличаются"
+    fi
 }
 
-# Создаем контрольные суммы для файлов в обеих директориях
-checksums1=$(generate_checksums "$dir1")
-checksums2=$(generate_checksums "$dir2")
-
-# Сравниваем контрольные суммы
-if [ "$checksums1" == "$checksums2" ]; then
-    echo "Все файлы в обеих директориях совпадают по содержимому"
-else
-    echo "Есть различия в содержимом файлов между двумя директориями"
-    echo "Файлы, отличающиеся по содержимому:"
-
-    # Найдем различия
-    diff <(echo "$checksums1") <(echo "$checksums2") | grep '^<' | awk '{print $2}'
-    diff <(echo "$checksums1") <(echo "$checksums2") | grep '^>' | awk '{print $2}'
-fi
-
-echo "Сравнение завершено"
+# Перебор всех файлов в первой директории
+for file1 in "$dir1"/*; do
+    # Перебор всех файлов во второй директории
+    for file2 in "$dir2"/*; do
+        compare_files "$file1" "$file2"
+    done
+done

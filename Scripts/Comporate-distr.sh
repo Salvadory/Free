0fdfceb493ef6d4017368187c6944b6676e9ed6c
Sergey <Salvadory@ya.ru>
2024-06-07 18:37:58 +0600

    Update Comporate-distr.sh

--- Scripts/Comporate-distr.sh
+++ Scripts/Comporate-distr.sh
@@ -4,12 +4,11 @@
 dir1="path/to/first/directory"
 dir2="path/to/second/directory"
 
-# Функция для сравнения файлов
+# Функция для сравнения содержимого файлов
 compare_files() {
     file1=$1
     file2=$2
 
-    # Сравнение содержимого файлов
     if cmp -s "$file1" "$file2"; then
         echo "Файлы $file1 и $file2 идентичны"
     else
@@ -17,10 +16,32 @@ compare_files() {
     fi
 }
 
-# Перебор всех файлов в первой директории
-for file1 in "$dir1"/*; do
-    # Перебор всех файлов во второй директории
-    for file2 in "$dir2"/*; do
-        compare_files "$file1" "$file2"
+# Функция для рекурсивного сравнения содержимого директорий
+compare_directories() {
+    local subdir1=$1
+    local subdir2=$2
+
+    # Перебор всех файлов и поддиректорий в первой директории
+    for entry in "$subdir1"/*; do
+        name=$(basename "$entry")
+        
+        if [ -d "$entry" ]; then
+            # Если это поддиректория, рекурсивно сравниваем
+            if [ -d "$subdir2/$name" ]; then
+                compare_directories "$entry" "$subdir2/$name"
+            else
+                echo "Поддиректория $subdir2/$name отсутствует"
+            fi
+        elif [ -f "$entry" ]; then
+            # Если это файл, сравниваем со всеми файлами во второй директории
+            for file2 in "$subdir2"/*; do
+                if [ -f "$file2" ]; then
+                    compare_files "$entry" "$file2"
+                fi
+            done
+        fi
     done
-done
+}
+
+# Запускаем сравнение для корневых директорий
+compare_directories "$dir1" "$dir2"
